bd431514678f1529bce2eefe8395e678638bcd75
KingOThePig <kingothepig@gmail.com>
2014-07-27 00:04:20 +1000

    added CreativeTab class and NBTHelper class from LetsModReboot

---
+++ src/main/java/com/kingothepig/tutorialmod/init/Recipes.java
@@ -0,0 +1,24 @@
+package com.kingothepig.tutorialmod.init;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
+
+public class Recipes {
+    public static void init(){
+        //Shaped Recipes
+        GameRegistry.addRecipe(new ItemStack(ModItems.mapleLeaf), "x x", " x ", " x ", 'x', new ItemStack(Items.stick));
+
+        //Shapeless Recipes
+        GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.flag), new ItemStack(ModItems.mapleLeaf));
+
+        //Ore Dictionary Shaped Recipes
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.mapleLeaf), "x x", " x ", " x ", 'x', "stickWood"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.mapleLeaf), "x", 'x', "treeLeaves"));
+
+        //Ore Dictionary Shapeless Recipes
+        //GameRegistry.addShapelessRecipe(new ShapelessOreRecipe(new ItemStack(ModBlocks.flag), new ItemStack(ModItems.mapleLeaf)));
+    }
+}
